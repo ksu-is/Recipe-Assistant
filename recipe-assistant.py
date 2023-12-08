535e57dbb558158c782c5f9d3a6e3bacaa08a48b
mmart379 <142547349+mmart379@users.noreply.github.com>
2023-12-08 08:02:00 -0500

    final recipe-assistant
    removed old lists, created dictionary
    finalized functions
    changed menu and removed accepted ingredients function
    bug tested

--- recipe-assistant.py
+++ recipe-assistant.py
@@ -1,69 +1,103 @@
-import os
+import random
 
-cmd ="curl https://raw.githubusercontent.com/ksu-is/Recipe-Assistant/main/accepted_ingredients.txt -o accepted_ingredients.txt" #link to list of ingredients the user is allowed to enter, currently incomplete
+user_ingredients_list = [] #list of ingredients the user has -- user will enter data into this list
 
-os.system(cmd) #opens the ingredients link, reads the content, and converts it into a list
-ingredients = open("accepted_ingredients.txt", "r")
-ingredients_text = ingredients.read()
-accepted_ingredients_list = ingredients_text.split("\n") #accepted ingredients list
-
-cmd1 ="curl https://raw.githubusercontent.com/ksu-is/Recipe-Assistant/main/recipes.txt -o recipes.txt" #link to list of recipes the program can suggest, currently incomplete
-
-os.system(cmd1) #opens the recipes link, reads the content, and converts it into a list
-recipes = open("recipes.txt", "r")
-recipes_text = recipes.read()
-recipes_list = recipes_text.split("\n") #recipes list
-
-#print(ingredients_list) tested these two to make sure they worked, and they did
-#print(recipes_list)
-
-user_ingredients_list = [""] #list of ingredients the user has -- user will enter data into this list
+recipes = { #dictionary of recipes this program can suggest
+ 'French Toast' : ['bread', 'eggs', 'syrup', 'butter'],
+ 'Spaghetti' : ['pasta', 'tomato sauce', 'ground beef', 'parmesan cheese'],
+ 'Fruit Salad' : ['apples', 'grapes', 'bananas', 'oranges'],
+ 'Chili' : ['ground beef', 'tomatoes', 'beans', 'chili powder'],
+ 'Cake' : ['flour', 'sugar', 'eggs', 'butter', 'oil'],
+ 'Guacamole' : ['avocados', 'lime juice', 'cilantro', 'tomatoes'],
+ 'Garlic Bread' : ['bread', 'butter', 'garlic'],
+ 'Mashed Potatoes' : ['potatoes', 'butter', 'milk'],
+ 'French Fries' : ['potatoes', 'oil'],
+ 'Scrambled Eggs' : ['eggs', 'milk', 'butter'],
+ 'Applesauce' : ['apples', 'sugar', 'cinnamon'],
+ 'Grilled Cheese' : ['bread', 'cheese', 'butter'],
+ 'Roasted Broccoli' : ['broccoli', 'oil', 'garlic'],
+ 'Hard-Boiled Eggs' : ['eggs', 'salt'],
+ 'Fried Chicken' : ['chicken', 'eggs', 'flour'],
+ 'Banana Bread' : ['bananas', 'flour', 'sugar', 'eggs'],
+ 'Egg Drop Soup' : ['eggs', 'chicken broth', 'cornstarch'],
+ 'BLT' : ['bread', 'bacon', 'lettuce', 'tomato', 'mayonnaise'],
+ 'Sushi' : ['rice', 'seaweed', 'fish'],
+ 'Chicken Quesadilla' : ['chicken', 'tortilla', 'cheese' ],
+ 'Mac and Cheese' : ['pasta', 'milk', 'cheese'],
+}
 
 def add_ingredients(): #adds ingredients to the user's list of ingredients
     while True:
-        ingredients_input = input('Enter an ingredient you have, or type "exit" to return to the main menu\n')
-        ingredients_input = ingredients_input.lower()
-        if ingredients_input=="exit":
-            return
-        elif ingredients_input in accepted_ingredients_list:
-            user_ingredients_list += ingredients_input #this doesnt work but i will solve this later
+        user_input = input("Enter an ingredient you have, or type X to return to the main menu.\n")
+        user_input = user_input.lower()
+        if user_input == "x":
+            return user_ingredients_list
         else:
-            display_accepted_ingredients = input("Sorry, I don't recognize that ingredient. Would you like to see a list of ingredients I accept? Type 'yes' or 'no'.\n")
-            display_accepted_ingredients = display_accepted_ingredients.lower()
-            if display_accepted_ingredients == "yes":
-                accepted_ingredients()
-            elif display_accepted_ingredients == "no":
-                print("Understood! Returning to ingredient input menu...")
-            else:
-                print("Sorry, I don't understand! Returning to ingredient input menu...")
+            user_ingredients_list.append(user_input)
 
+def suggest_recipe(user_ingredients_list): #suggests a recipe using ingredients the user has
+    suggested_recipes = []
 
-def accepted_ingredients(): #prints a list displaying the ingredients the program accepts
-    print(accepted_ingredients_list)
+    for recipe, ingredients in recipes.items():
+        if all(ingredient in user_ingredients_list for ingredient in ingredients):
+            suggested_recipes.append(recipe)
 
-def suggest_recipe(): #suggests a recipe using ingredients the user has -- **plan to add an option that says something along the lines of "dont like? let me suggest a different recipe!"**
-    print()
+    if suggested_recipes:
+            print("You can make the following recipes:")
+            for recipe in suggested_recipes:
+                print(recipe)
+    else:
+        print("Sorry, no recipes found with the given ingredients.")    
 
 def all_recipes(): #prints all recipes the program knows by name of recipe, user will then have the option to select a recipe to see more details
-    print()
+    n = 1
+    for x in recipes:
+        print(f"{n}. {x}")
+        n += 1
+    see_more = input("Would you like to see more details about any recipe that was shown? Type 'Yes' or 'No.'\n")
+    see_more = see_more.lower()
+    if see_more == "yes":
+
+        which_recipe = input("What is the name of the recipe you would like to see more about?\n")
+        for a, b in recipes.items():
+            if a.lower() == which_recipe.lower():
+                print(f"{a}: You will need {b}")
+                break
+            else:
+                continue
+    
+    elif see_more == "no":
+        return
+    else:
+        print("Sorry, I don't understand. Returning to main menu.")
+        return
 
 def random_recipe(): #prints a random recipe
-    print()
+    x1, x2 = random.choice(list(recipes.items()))
+    print(f"{x1}: You will need {x2}")
+    another_random = input("Would you like to see another random recipe? Type 'Yes' or 'No'.\n")
+    another_random = another_random.lower()
+    if another_random == "yes":
+        random_recipe()
+    elif another_random == "no":
+        return
+    else: 
+        print("Sorry, I don't understand. Returning to main menu.")
+        return
+
 
 print("Welcome chef! I am your personal recipe assistant. Let's get to cooking!")
 while True: #prints the menu where users can select how they want to navigate the program, until user ends the program with option 6. options run one of the above functions
-    menu_selection = input("How can I help you plan your next meal?\n1. Add Ingredients\n2. View Accepted Ingredients\n3. Suggest a Recipe\n4. View All Recipes\n5. Random Recipe\n6. Exit Program\n")
+    menu_selection = input("How can I help you plan your next meal?\n1. Add Ingredients\n2. Suggest a Recipe\n3. View All Recipes\n4. Random Recipe\n5. Exit Program\n")
     if menu_selection=="1":
         add_ingredients()
     elif menu_selection=="2":
-        accepted_ingredients()
+        suggest_recipe(user_ingredients_list)
     elif menu_selection=="3":
-        suggest_recipe()
-    elif menu_selection=="4":
         all_recipes()
-    elif menu_selection=="5":
+    elif menu_selection=="4":
         random_recipe()
-    elif menu_selection=="6":
+    elif menu_selection=="5":
         break
     else:
-        print("Invalid menu selection. Chef, please choose a number 1-6:")
+        print("Invalid menu selection. Chef, please choose a number 1-5:")
